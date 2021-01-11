f17a73ec3534d1af526ea2d34fc3ca0dd3d0aa5c
vivekkumar17599 <76811238+vivekkumar17599@users.noreply.github.com>
2021-01-11 05:02:48 -0800

    Update deleteStudent.dart

--- lib/deleteStudent.dart
+++ lib/deleteStudent.dart
@@ -4,9 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project/AddStudent.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
-//import 'Homepage.dart';
-//import 'main.dart';
-
 
 class deleteStudent extends StatefulWidget {
 
@@ -17,34 +14,11 @@ class deleteStudent extends StatefulWidget {
 class _deleteStudentState extends State<deleteStudent> {
   Query ref;
 
-  //DatabaseReference rf;
-  //String key;
- //DeleteRecord(){
-    //Widget okButton=FlatButton(
-      //child: Text("OK"),
-     // onPressed: (){
-       // setState(() {
-         // rf.child('user.id').remove();
-       // });
-     // };
-    //);
-  //}
   @override
   void initState(){
     super.initState();
     ref=FirebaseDatabase.instance.reference().child('Students').orderByChild('SName');
-
-   // rf=FirebaseDatabase.instance.reference().child('Students');
-    //key= rf.child('Students');
-   // key= rf.child('Students').push().getKey();
-   //// ref.once().then((DataSnapshot snap ){
-     // var data=snap.value;
-
-   // }
-   // );
   }
-
-
 
   Widget _deleteStudent({Map Students}){
    return SingleChildScrollView(
@@ -78,13 +52,6 @@ ListTile(
                fontSize: 18.0
             ),),
             onPressed: (){
-            //  DeleteRecord();
-              // setState(() {
-
-
-            // rf.child().remove('Students');
-            // Navigator.of(context).pop();
-             // });
              },
           ),
          ],
